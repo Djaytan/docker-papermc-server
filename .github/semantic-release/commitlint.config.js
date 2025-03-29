8f36883abd60ddcca62b82d16002dbd5110bfaaf
Djaytan <26904516+Djaytan@users.noreply.github.com>
2025-03-29 14:35:20 +0100

    ci: enforce conventional commits

---
+++ .github/semantic-release/commitlint.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    // Project specific rules
+    'scope-enum': [2, 'always', [
+      'readme', 'contributing', 'packaging', 'deps', 'other', 'github',
+      'renovate', 'release'
+    ]],
+
+    // Compliant with Angular conventions (get rid of "style" type but tolerate "chore" one)
+    'type-enum': [2, 'always', [
+      'feat', 'fix', 'perf', 'revert', 'refactor', 'build', 'test', 'ci',
+      'docs', 'chore'
+    ]],
+
+    // Adjust some rules predefined by @commitlint/config-conventional
+    'body-max-line-length': [0, 'always', 'Infinity'],
+    'footer-max-line-length': [0, 'always', 'Infinity']
+  }
+}
