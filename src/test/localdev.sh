f08a2aa743bcb525f83d12865c7ecc31208ed0f4
Djaytan <26904516+Djaytan@users.noreply.github.com>
2025-04-08 00:05:26 +0200

    feat: include standard OCI annotations on published images (#54)
    For example:
    
    ```
    "org.opencontainers.image.title=PaperMC Server",
    "org.opencontainers.image.description=Dockerized and fine-grained customizable PaperMC server.",
    "org.opencontainers.image.version=1.21.4-v1.0.0-20250407",
    "org.opencontainers.image.url=https://github.com/Djaytan/docker-papermc-server",
    "org.opencontainers.image.documentation=https://github.com/Djaytan/docker-papermc-server",
    "org.opencontainers.image.source=https://github.com/Djaytan/docker-papermc-server.git",
    "org.opencontainers.image.authors=Djaytan \u003chttps://github.com/Djaytan\u003e",
    "org.opencontainers.image.vendor=Djaytan",
    "org.opencontainers.image.licenses=GPL-3.0-or-later",
    "org.opencontainers.image.created=2025-04-07T21:41:44Z",
    "org.opencontainers.image.revision=e0f3d523b9c670409e04225b737a838f0391d8b7"
    ```

--- src/test/localdev.sh
+++ src/test/localdev.sh
@@ -7,6 +7,5 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 bash "$SCRIPT_DIR"/build-dev.sh
 
 echo '▶️ Starting the localdev PaperMC server...'
-# TODO: migrate to Docker compose?
 docker run --rm -it -p 25565:25565/tcp -p 25565:25565/udp -e EULA=true 'djaytan/papermc-server:dev'
 echo '🛑 The localdev PaperMC server has been stopped and removed.'
