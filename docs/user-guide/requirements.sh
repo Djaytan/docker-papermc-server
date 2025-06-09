2c194e807dc6100d5d7e3e7fb804b65d9d936da7
Djaytan <26904516+Djaytan@users.noreply.github.com>
2025-06-09 18:59:41 +0200

    build: reorganize pip-compile wrapper files (#250)

--- docs/user-guide/requirements.sh
+++ docs/user-guide/requirements.sh
@@ -5,7 +5,8 @@ set -eu
 
 SCRIPT_DIR=$(cd "$(dirname "$0")" > /dev/null 2>&1 && pwd -P)
 
-echo 'Compile the requirements.txt file using pip-tools...'
+echo 'Compile the requirements.txt file using pip-compile'
+echo
 
 cd "$SCRIPT_DIR"
-sh wrappers/pip-tools/pip-compile
+sh pip-compile/pip-compile.sh --generate-hashes
