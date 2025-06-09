a45ce3508fdd567aa8a1d57287876f19351858d6
Djaytan <26904516+Djaytan@users.noreply.github.com>
2025-06-09 18:28:45 +0200

    build: fix user guide localdev script

--- docs/user-guide/localdev.sh
+++ docs/user-guide/localdev.sh
@@ -7,4 +7,4 @@ SCRIPT_DIR=$(cd "$(dirname "$0")" > /dev/null 2>&1 && pwd -P)
 echo 'The preview is going to be available at http://localhost:8000/docker-papermc-server/'
 echo
 
-sh "${SCRIPT_DIR}/mkdocs" serve --dev-addr '0.0.0.0:8000'
+sh "${SCRIPT_DIR}/mkdocs.sh" serve --dev-addr '0.0.0.0:8000'
